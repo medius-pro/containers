74b511851dff2afce0876304d5a8939925640bd8
Tomasz Sądej <tomasz@medius.pro>
2024-05-06 19:56:36 +0200

    refactor: remove tls

--- php/erp/build.sh
+++ php/erp/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-docker build --no-cache --pull --platform "linux/arm64" -t ghcr.io/medius-pro/php:erp .
+docker build --no-cache --pull --platform "linux/arm64" -t medius/php:erp .
